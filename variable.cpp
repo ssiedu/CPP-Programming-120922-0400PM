d5a7992efd23ff92af87ef43c5daa0f3e28ebcb5
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-09-17 12:36:36 +0530

    variable

---
+++ variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int a=10;
+	cout<<"\n value of a : "<<a;
+	int A1=20;
+	cout<<"\n value of a : "<<A1;
+	int If=45;
+	cout<<"\n new value of a : "<<If;
+	return 0;
+}
